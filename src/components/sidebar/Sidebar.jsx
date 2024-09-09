dddfa43106a8f5d4389b6960987149a319b7cf79
Rodolpho Reis <rodolphoreis@live.com>
2024-09-09 20:17:09 +0100

    chore: change img cover

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,9 +6,10 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         className={styles.cover}
-        src="https://images.pexels.com/photos/5483071/pexels-photo-5483071.jpeg?auto=compress&cs=tinysrgb&w=560&h=350&dpr=2"
+        src="https://media.licdn.com/dms/image/v2/D4D16AQHDoIcifC6Wtg/profile-displaybackgroundimage-shrink_350_1400/profile-displaybackgroundimage-shrink_350_1400/0/1718380155312?e=1731542400&v=beta&t=8d25BhpGHw7nT10UlFiIKsfI8__P2PmEOTYog5A9uMg"
       />
       <div className={styles.profile}>
+        <img src="https://avatars.githubusercontent.com/u/135350341?v=4&size=64" />
         <strong className={styles.name}>Rodolpho Reis</strong>
         <span className={styles.job}>Web Developer</span>
       </div>
