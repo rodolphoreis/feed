eea3b6651173e0de24991e4a214a5819b19a5527
Rodolpho Reis <rodolphoreis@live.com>
2024-09-09 15:51:58 +0100

    refactor: remove unused autor prop and eslint-disable comment from APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-/* eslint-disable react/prop-types */
 import { Header } from "./components/header/Header";
 import "./styles/styles.css";
-export function App({ autor }) {
-  console.log(autor);
+export function App() {
   return (
     <>
       <Header />
